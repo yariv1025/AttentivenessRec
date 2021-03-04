6a217a1bbb78435a0ff8db01cafc30e83ecf4c01
StavLobel <44868312+StavLobel@users.noreply.github.com>
2021-03-04 15:08:09 +0200

    Added comments

--- src/FrameProvider.py
+++ src/FrameProvider.py
@@ -2,11 +2,21 @@ import cv2
 
 
 class FrameProvider(cv2.VideoCapture):
+    """
+        Constructor:
+        :param param - Video stream source.
+        :return Boolean - If stream successfully initialized.
+        """
 
     def __init__(self, param):
         super().__init__(param)
         if not self.isOpened():
             raise IOError("Could not open video stream.")
+        return True
+
+    """    
+    Get frame from a video stream, if stream is open.
+    """
 
     def get_frame(self):
         if not self.isOpened():
