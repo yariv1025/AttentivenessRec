150792beedb5ddc163a01d0ce3fd55218f878ae0
Yariv Garala <yariv1052@gmail.com>
2021-03-15 19:42:18 +0200

    Fix: docs

--- src/FrameProvider.py
+++ src/FrameProvider.py
@@ -2,22 +2,22 @@ import cv2
 
 
 class FrameProvider(cv2.VideoCapture):
-    """
+
+    def __init__(self, param):
+        """
         Constructor:
         :param param - Video stream source.
         :return Boolean - If stream successfully initialized.
         """
-
-    def __init__(self, param):
         super().__init__(param)
         if not self.isOpened():
             raise IOError("Could not open video stream.")
 
-    """    
-    Get frame from a video stream, if stream is open.
-    """
-
     def get_frame(self):
+        """
+         Get frame from a video stream, if stream is open.
+        :return: frame
+        """
         if not self.isOpened():
             raise IOError("Could not open video stream.")
         res, frame = self.read()
--- src/Location.py
+++ src/Location.py
@@ -1,22 +1,20 @@
-"""
-:return Boolean
-checks for valid value
-"""
-
-
 def check_value(value):
+    """
+    checks for valid value
+    :param value: integer (0-255)
+    :return: Boolean
+    """
     if (not isinstance(value, int)) or value < 0:
         return False
     return True
 
 
-"""
-:return Boolean
-checks for valid values
-"""
-
-
 def check_multi_value(*integers):
+    """
+    checks for multi valid values
+    :param integers: integers (value: 0-255)
+    :return: boolean
+    """
     for x in integers:
         if not check_value(x):
             return False
@@ -24,15 +22,15 @@ def check_multi_value(*integers):
 
 
 class Location:
-    """
-    Constructor:
-    :param x - x coordinate of the upper right corner (0 on default).
-    :param y - y coordinate of the upper right corner (0 on default).
-    :param width - frame width (0 on default)
-    :param length - frame length (0 on default)
-    """
 
     def __init__(self, x=0, y=0, width=0, length=0):
+        """
+        Constructor:
+        :param x - x coordinate of the upper right corner (0 on default).
+        :param y - y coordinate of the upper right corner (0 on default).
+        :param width - frame width (0 on default)
+        :param length - frame length (0 on default)
+        """
         if not check_multi_value(x, y, width, length):
             raise IOError("Invalid values")
         self.x = x
@@ -40,60 +38,63 @@ class Location:
         self.width = width
         self.length = length
 
-    """
-    :param int - x coordinate of the upper right corner.
-    """
-
     def set_x(self, x):
+        """
+        Set x coordinate value.
+        :param int - x coordinate of the upper right corner.
+        """
         if not check_value(x):
             raise IOError("Invalid values")
         self.x = x
         return True
 
-    """
-    :param int - y coordinate of the upper right corner.
-    """
-
     def set_y(self, y):
+        """
+        reset y coordinate of the upper right corner.
+        :param y: integer - coordinate of the upper right corner.
+        :return: boolean
+        """
         if not check_value(y):
             raise IOError("Invalid values")
         self.y = y
         return True
 
-    """
-    :param int - frame width.
-    """
-
     def set_width(self, width):
+        """
+        reset the width of the face box.
+        :param width: integer - width of the face box
+        :return: boolean
+        """
         if not check_value(width):
             raise IOError("Invalid values")
         self.width = width
         return True
 
-    """    
-    :param int - frame length.
-    """
-
     def set_length(self, length):
+        """
+        reset the length of the face box.
+        :param length: integer - length of the face box
+        :return: boolean
+        """
         if not check_value(length):
             raise IOError("Invalid values")
         self.length = length
         return True
 
-    """
-    :return - returns Location object (x,y, width, length).
-    """
-
     def get_location(self):
+        """
+        gets the Location object (x,y, width, length).
+        :return: Location
+        """
         return {'x': self.x,
                 'y': self.y,
                 'width': self.width,
                 'length': self.length
                 }
 
-    """
-    :return - returns Location as a string.
-    """
-
     def to_string(self):
+        """
+        create a readable string of Location object.
+        :return: object string
+        """
         return '({0}, {1}, {2}, {3})'.format(self.x, self.y, self.width, self.length)
