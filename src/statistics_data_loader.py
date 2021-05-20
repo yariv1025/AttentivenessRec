add94c81057d6c4222e7461fa74e3af46b382c8e
StavLobel <44868312+StavLobel@users.noreply.github.com>
2021-05-20 16:29:13 +0300

    Refactor of statistics_data_loader.py

--- src/statistics_data_loader.py
+++ src/statistics_data_loader.py
@@ -2,17 +2,33 @@ from pandas import DataFrame
 
 
 class Statistics(object):
+    """
+    A class for storing the attention values of the subject.
+    """
 
     def __init__(self):
+        """
+        Creating a new Statistics object.
+        """
         self.times = []
         self.values = []
 
     def addValue(self, time, value):
+        """
+        A function for adding a new row for the attention level table.
+
+        :param time: the time which the attention level has been recorded.
+        :param value: the attention level.
+        """
         self.times.append(time)
         self.values.append(value)
-        # self.get_data_frame()
 
     def get_data_frame(self):
+        """
+        A function for recieving the attention levels that has been recorded as a DataFrame object.
+
+        :return: Dataframe object of the attention levels.
+        """
         data = {'Time': self.times,
                 'Emotion levels': self.values
                 }
