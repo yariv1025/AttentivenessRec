0ee052bec7dafb6ecbf6286da7ad2ddafc4af46f
StavLobel <44868312+StavLobel@users.noreply.github.com>
2021-05-20 14:12:17 +0300

    Refactor of app.py

--- src/app.py
+++ src/app.py
@@ -1,36 +1,45 @@
-from src.FrameProvider import FrameProvider
-from src.FrameSaver import FrameSaver
 import threading
-from src.emotic_loop import EmoticLoop
 import src.gui
 import tkinter as tk
+from src.FrameProvider import FrameProvider
+from src.FrameSaver import FrameSaver
 from src.statistics_data_loader import Statistics
+from src.emotic_loop import EmoticLoop
 
 
 def app():
+    # TODO: fix closing of the threads.
+
     """
-    TODO: ??
+    The main function for the app.
+    This function creates the locks for the multi-threading execution, starts the threads
+    and creating the GUI window.
     """
-    exit_flag = True
 
     # locks creation
     p_lock = threading.Lock()
     c_lock = threading.Lock()
     locks = [p_lock, c_lock]
 
+    # creation of misc variables for the app
     fp = FrameProvider(0)
     statistics = Statistics()
     window = tk.Tk()
+    exit_flag = True
 
     # Create a gui window and pass it to the Application object
     gui = src.gui.App(window, "AttentivnessRec", statistics, exit_flag, fp)
 
+    # create the 2 other threads of the app
     thread_saver = FrameSaver(2, fp, locks, gui, exit_flag)
     emotic_loop = EmoticLoop(3, fp, locks, gui, exit_flag)
 
+    # initializing of the threads
     thread_saver.start()
     emotic_loop.start()
     gui.start()
 
 
-app()
+# calling the app
+if __name__ == "__main__":
+    app()
