6ceed0bd7860c953f16f1caefe691afd2c6f377e
StavLobel <44868312+StavLobel@users.noreply.github.com>
2021-05-20 14:32:37 +0300

    Refactor of FaceDetector.py

--- src/FaceDetector.py
+++ src/FaceDetector.py
@@ -2,26 +2,32 @@ from mtcnn.mtcnn import MTCNN
 
 
 class FaceDetector(MTCNN):
-
+    """
+    Perform facial detection.
+    """
     def __init__(self):
         """
-        Initialize - calling to super class.
+        Creating FaceDetector object by calling to the super class constructor (MTCNN).
         """
         super().__init__()
 
     def get_face(self, img):
         """
-        Detects bounding boxes from the specified image.
-        :param img: image to process.
-        :return: list containing all the bounding boxes detected with their keypoints.
+        Detects faces in an image.
+        If faces has been detected, returning the bounding boxes for the faces relative to the specified image.
+        Else, return an empty list.
+
+        :param img: image/frame to process.
+        :return: list containing all the bounding boxes detected with their key-points.
         """
         return self.detect_faces(img)
 
     def has_face(self, img):
         """
-        face verification in the image.
-        :param img: image to process.
-        :return: boolean
+        Face verification within an image.
+
+        :param img: image/frame to process.
+        :return: boolean value - False if theres no face. Else, True.
         """
         faces = self.get_face(img)
         return False if len(faces) == 0 else faces
