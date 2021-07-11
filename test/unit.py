2056e9cc5391c157df85effa108b044777f2cd02
Yariv Garala <yariv1052@gmail.com>
2021-07-11 11:55:06 +0300

    Adding unit tests for FaceDetector, FrameSaver, Statistics and FrameProvider classes

--- test/unit.py
+++ test/unit.py
@@ -1,135 +1,141 @@
 import unittest
-from matplotlib import pyplot
-from mtcnn.exceptions.invalid_image import InvalidImage
+import os
+import threading
+from datetime import datetime
+import pandas
+import cv2
+import numpy
 
-from src.Location import Location
-
-
-
-class LocationTestCase(unittest.TestCase):
-
-    def test_setter(self):
-        location = Location()
-
-        location.set_x(1)
-        self.assertEqual(location.x, 1)
-
-        with self.assertRaises(IOError):
-            location.set_y(-2)
-
-        with self.assertRaises(IOError):
-            location.set_width('#')
-
-    def test_to_string(self):
-        location = Location(1, 2, 3, 4)
-        self.assertEqual(location.get_location(), {'x': 1, 'y': 2, 'width': 3, 'length': 4})
-
-    def test_constructor(self):
-        with self.assertRaises(IOError):
-            Location('a', 'b', 'c', 'd')
+from src.class_details import ClassDetails
+from src.frame_saver import FrameSaver
+from src.statistics_data_loader import Statistics
+from src.face_detector import FaceDetector
+from src.frame_provider import FrameProvider
 
 
+class FrameProviderTestCase(unittest.TestCase):
+    fp = FrameProvider(0)
 
-# # Example:
-#
-# class MyTestCase(unittest.TestCase):
-#
-#     def test_first(self):
-#         c = Collection((3, 4, 5, 7, 8, 9))
-#         self.assertEqual(c.first(), 3)
-#
-#     def test_last(self):
-#         c = Collection((3, 4, 5, 7, 8, 9))
-#         self.assertEqual(c.last(), 9)
-#
-#     def test_take(self):
-#         c = Collection((3, 4, 5, 7, 8, 9))
-#         self.assertTupleEqual(c.take(2).values(), (3, 4))
-#
-#     def test_append(self):
-#         c = Collection((3, 4, 5, 7, 8, 9))
-#         self.assertEqual(c.append(10,11,12), Collection((3, 4, 5, 7, 8, 9, 10 ,11, 12)))
-#
-#     def test_prepend(self):
-#         c = Collection((3, 4, 5, 7, 8, 9))
-#         self.assertEqual(c.prepend(0, 1, 2), Collection((0,1,2,3,4,5,7,8,9)))
-#
-#     def test_filter(self):
-#         c = Collection((3, 4, 5, 7, 8, 9))
-#         self.assertEqual(c.filter(lambda x: x % 2 == 0).Iterable,(4,8))
-#
-#     def test_map(self):
-#         c = Collection((3,4,5,7,8,9))
-#         self.assertEqual(c.map(lambda x: x + 2).Iterable, (5, 6, 7, 9, 10, 11))
-#
-#     def test_reduce(self):
-#         c = Collection((3, 4, 5, 7, 8, 9))
-#         self.assertEqual(c.reduce(lambda x,y: x + y, 0), 36)
-#
-#     def test_sort(self):
-#         c1 = Collection((1, 2, 3, 4))
-#         self.assertEqual(c1.sort(key = lambda x: x + 2 , reversed = False), Collection(3,4,5,6))
-#         #TODO: FIX : Sort by index and not by lambda!
-#
-#     def test_set(self):
-#         c = Collection((3, 4, 5, 7, 8, 9))
-#         self.assertEqual(c.set(3,{'a' : 1, 'b' : 2}).values(), (3, 4, 5, {'a' : 1, 'b' : 2}, 8, 9))
-#
-#     def test_pluck(self):
-#         c = Collection([{'name': 'Joe', 'age': 20}, {'name': 'Jane', 'age': 13}])
-#         self.assertEqual(c.pluck('age'), (20,13))
-#
-#     def test_values(self):
-#         c = Collection(('a', 'b', 'c', 'd'))
-#         self.assertEqual(c.values(), ('a', 'b', 'c', 'd'))
-#
-#     def test_unique(self):
-#         c1 = Collection((4,3,4,5,5,7,8,9))
-#         self.assertTupleEqual((c1.unique()).values(), (4, 3, 5, 7, 8, 9))
-#
-#     def test_tap(self):
-#         c = Collection((1,2,3,4))
-#         callback = lambda x : x + 2
-#         self.assertEqual(c.tap(callback),None)
-#
-#     def test_getitem(self):
-#         c = Collection(("A", "B", "C"))
-#         self.assertEqual(c.__getitem__(1), 'B')
-#
-#     def test_add(self):
-#         c = Collection((1,2,3,4))
-#         b = Collection(('abcd'))
-#         self.assertEqual(c.__add__(b), Collection((1, 2, 3, 4, 5, 'a', 'b', 'c', 'd', 'e')))
-#
-#     def test_sub(self):
-#         c = Collection((1,2,3,4))
-#         self.assertEqual(c.__sub__((3,4,5,6)), (1,2))
-#
-#     def test_len(self):
-#         c = Collection((1,2,3,4))
-#         self.assertEqual(len(c), 4)
-#
-#     def test_contains(self):
-#         c = Collection(('a', 'b', 'c'))
-#         self.assertEqual(c.__contains__('a'), True)
-#
-#     def test_eq(self):
-#         c = Collection((1,2,3,4))
-#         self.assertEqual(c.__eq__((1,2,3,4)), True)
-#
-#     def test_ne(self):
-#         #self.assertEqual(not(TESTEQ.test_eq((1,2,3,4,5))) , False)
-#         c = Collection((1,2,3,4,5))
-#         self.assertEqual(c.__ne__((1,2,3,4,5)) , False)
-#
-#     def test_str(self):
-#         c = Collection(('a', 'b', 'c', 'd'))
-#         self.assertEqual(c.__str__(), ('a', 'b', 'c', 'd'))
-#
-#     def test_repr(self):
-#         c = Collection((1,2,3,4))
-#         self.assertEqual(repr(c), "Collection(1,2,3,4)")
-#
+    def test_get_frame(self):
+        frame = self.fp.get_frame()
+        expected_type = type(numpy.ndarray([]))
+
+        self.assertEqual(expected_type, type(frame))
+
+
+class FaceDetectorTestCase(unittest.TestCase):
+    fd = FaceDetector()
+
+    main_path = os.path.dirname(os.getcwd())
+    img_with_face = cv2.imread(main_path + "\public\\test_img\with_face.png")
+    img_without_face = cv2.imread(main_path + "\public\\test_img\without_face.png")
+
+    def test_get_face(self):
+        result = self.fd.get_face(self.img_with_face)
+        expected = True
+
+        self.assertEqual(expected, len(result) > 0)
+
+    def test_get_no_face(self):
+        result = self.fd.get_face(self.img_without_face)
+        expected = 0
+
+        self.assertEqual(expected, len(result))
+
+    def test_has_face(self):
+        result = self.fd.has_face(self.img_with_face)
+        expected = True
+        self.assertEqual(expected, len(result) > 0)
+
+    def test_has_no_face(self):
+        result = self.fd.has_face(self.img_without_face)
+        expected = False
+        self.assertEqual(expected, result)
+
+    def test_start_timer(self):
+        result = self.fd.start_timer()
+        expected = type(datetime.now())
+        self.assertEqual(expected, type(result))
+
+    def test_stop_timer(self):
+        result = self.fd.stop_timer()
+        expected = None
+
+        self.assertEqual(expected, result)
+
+
+class StatisticsTestCase(unittest.TestCase):
+    class_data = ClassDetails(lecture_value=1, lecturer_value='Tamar Shrot')
+    fd = FaceDetector()
+    stat = Statistics(class_data, fd)
+
+    def test_add_value(self):
+        val = self.stat.values.copy()
+        time = self.stat.times.copy()
+        self.stat.add_value(datetime.now(), 10)
+
+        expected_time = len(time) + 1
+        expected_val = len(val) + 1
+
+        time_result = len(self.stat.times)
+        val_result = len(self.stat.values)
+
+        self.assertEqual(expected_time, time_result)
+        self.assertEqual(expected_val, val_result)
+
+    def test_save_emotion(self):
+        emotion_counter = self.stat.emotion_counter.copy()
+        self.stat.save_emotion(['Affection'])
+
+        expected = emotion_counter['Affection'] + 1
+        result = self.stat.emotion_counter['Affection']
+
+        self.assertEqual(expected, result)
+
+    def test_get_top_3(self):
+        emotions1 = ['Affection', 'Disconnection', 'Annoyance', 'Fatigue', 'Aversion']
+        emotions2 = ['Affection', 'Disconnection', 'Annoyance', 'Doubt/Confusion']
+        emotions3 = ['Disconnection', 'Excitement', 'Affection', 'Fear', 'Happiness', ]
+        self.stat.save_emotion(emotions1)
+        self.stat.save_emotion(emotions2)
+        self.stat.save_emotion(emotions3)
+
+        expected = sorted(['Disconnection', 'Affection', 'Annoyance'])
+        result = sorted(self.stat.get_top_3())
+
+        self.assertEquals(expected, result)
+
+    def test_get_data_frame(self):
+        expected = pandas.DataFrame()
+        result = self.stat.get_data_frame()
+
+        self.assertEqual(type(expected), type(result))
+
+
+class FrameSaverTestCase(unittest.TestCase):
+    # locks creation
+    p_lock = threading.Lock()
+    c_lock = threading.Lock()
+    locks = [p_lock, c_lock]
+
+    main_path = os.path.dirname(os.getcwd())
+    img_path = main_path + "\debug_exp\\frames\\frame.jpg"
+    img = cv2.imread(main_path + "\public\\test_img\\frame.png")
+
+    fs = FrameSaver(2, None, locks, None, None)
+
+    def test_save_frame(self):
+
+        try:
+            os.remove(self.img_path)
+        except FileNotFoundError:
+            pass
+
+        self.fs.save_frame(self.img)
+        result = os.path.isfile(self.img_path)
+
+        self.assertTrue(result)
+
 
 if __name__ == '__main__':
     unittest.main()
